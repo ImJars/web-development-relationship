69dd5f37b4d5c80043b04c9333a0af982139c4a3
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2024-10-16 19:18:30 -0600

    Update homeBackground

---
+++ src/components/homeBackground.jsx
@@ -0,0 +1,140 @@
+import React from "react";
+
+function HomeBackground() {
+  return (
+    <>
+      <div>
+        {/* svg */}
+        <svg
+          fill="none"
+          viewBox="0 0 538 404"
+          xmlns="http://www.w3.org/2000/svg"
+          role="img"
+          className="vector-home vector-home--corner-stain"
+        >
+          <path
+            fill="#3B57F5"
+            d="M77.319 350.968C32.815 336.341-100 291.765-100 291.765V-99.208C-100-129.39-75.662-154-45.298-154h290.433l.927 1.857C282.221-69.955 372.851 2.714 440.765 61.685c84.603 73.598 130.961 162.519 68.146 264.905-83.676 136.283-328.909 58.042-431.592 24.378Z"
+          ></path>
+        </svg>
+        {/* svg */}
+        <svg
+          fill="none"
+          viewBox="0 0 538 474"
+          xmlns="http://www.w3.org/2000/svg"
+          role="img"
+          className="vector-home vector-home--corner-stain animate-home-bubble2"
+        >
+          <path
+            fill="#398BF9"
+            d="M94.03 428.34C51.155 415.811-77 377.528-77 377.528V41.1C-77 15.114-53.362-6-24.394-6h280.182l.696 1.624c34.993 70.766 122.362 133.179 187.714 183.991 81.575 63.11 126.302 139.676 65.817 227.844-80.648 117.169-317.03 49.884-415.986 20.881Z"
+            opacity="0.2"
+          ></path>
+        </svg>
+        {/* svg */}
+        <svg
+          fill="none"
+          viewBox="0 0 597 890"
+          xmlns="http://www.w3.org/2000/svg"
+          role="img"
+          className="vector-home left-0 max-w-2xl w-1/2 sm:block md:w-2/5 duration-[5000]"
+        >
+          <path
+            stroke="#EB4AAE"
+            strokeDasharray="1759.962158203125 1759.962158203125"
+            strokeDashoffset="1759.962158203125"
+            strokeMiterlimit="10"
+            d="M596.278 1382.9c-5.727-5.73-566.402-192.47-410.206-588.85C342.268 397.447 223.083 218.635-112 1.285"
+            className="animate-lineLeft1"
+          ></path>
+        </svg>
+        {/* svg */}
+        <svg
+          fill="none"
+          viewBox="0 0 571 1024"
+          xmlns="http://www.w3.org/2000/svg"
+          role="img"
+          className="vector-home ml-28 left-28 max-w-xl w-[65%] sm:block md:w-[55%] lg:w-[45%] xl:w-2/5 xl:ml-44 xl:left-44"
+        >
+          <path
+            stroke="#EB4AAE"
+            strokeDasharray="1894.684814453125 1894.684814453125"
+            strokeDashoffset="1894.684814453125"
+            strokeMiterlimit="10"
+            d="M136.119-284s148.265 338.467-65.21 763.918c-213.475 425.451 90.545 739.692 499.43 742.112"
+            className="animate-lineLeft2"
+          ></path>
+        </svg>
+        {/* svg */}
+        <svg
+          fill="none"
+          viewBox="0 0 366 1024"
+          xmlns="http://www.w3.org/2000/svg"
+          role="img"
+          className="vector-home lg:block ml-36 xl:ml-56 left-36 xl:left-56 max-w-sm w-[35%] lg:w-[31%] xl:w-1/4"
+        >
+          <path
+            stroke="#EB4AAE"
+            strokeDasharray="1524.201904296875 1524.201904296875"
+            strokeDashoffset="1524.201904296875"
+            strokeMiterlimit="10"
+            d="M133.036-181.161s151.789 224.177 16.963 485.789C15.173 566.241-185.304 857.362 365.236 1120.08"
+            className="animate-lineLeft3"
+          ></path>
+        </svg>
+        <svg
+          fill="none"
+          viewBox="0 0 279 1024"
+          xmlns="http://www.w3.org/2000/svg"
+          role="img"
+          className="vector-home lg:block mr-24 right-20 max-w-[277px] w-2/5 md:w-[35%] lg:w-1/4 xl:w-1/5"
+        >
+          <path
+            stroke="#EB4AAE"
+            strokeDasharray="1431.4698486328125 1431.4698486328125"
+            strokeDashoffset="1431.4698486328125"
+            strokeMiterlimit="10"
+            d="M278.357 1072.94c-2.491-5.86-444.34-286.628-207.667-536.474C307.363 286.621 271.95 116.741 86.17-146.788"
+            className="animate-lineRight1"
+          ></path>
+        </svg>
+        {/* svg */}
+        <svg
+          fill="none"
+          viewBox="0 0 328 1024"
+          xmlns="http://www.w3.org/2000/svg"
+          role="img"
+          className="vector-home sm:block right-0 max-w-xs w-[45%] md:w-2/5 lg:w-[28%] xl:w-[22.71%]"
+        >
+          <path
+            stroke="#EB4AAE"
+            strokeDasharray="1529.386474609375 1529.386474609375"
+            strokeDashoffset="1529.386474609375"
+            strokeMiterlimit="10"
+            d="M439.882-169s9.075 298.002-279.203 552.29c-288.279 254.287-154.995 581.076 150.545 706"
+            className="animate-lineRight2"
+          ></path>
+        </svg>
+        {/* svg */}
+        <svg
+          fill="none"
+          viewBox="0 0 205 1024"
+          xmlns="http://www.w3.org/2000/svg"
+          role="img"
+          className="vector-home sm:block right-0 max-w-[210.74px] w-[31%] md:w-1/4 xl:w-1/5"
+        >
+          <path
+            stroke="#EB4AAE"
+            strokeDasharray="1230.516357421875 1230.516357421875"
+            strokeDashoffset="1230.516357421875"
+            strokeMiterlimit="10"
+            d="M386.994-19.021s46.267 213.594-133.818 368.903C73.091 505.192-164.828 662.633 168.65 1025.14"
+            className="animate-lineRight3"
+          ></path>
+        </svg>
+      </div>
+    </>
+  );
+}
+
+export default HomeBackground;
