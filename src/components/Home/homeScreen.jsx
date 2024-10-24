5aa19fd2998027898e23537eb0dbe9db4e157ffb
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2024-10-23 23:36:11 -0600

    Update homeScreen.jsx

--- src/components/Home/homeScreen.jsx
+++ src/components/Home/homeScreen.jsx
@@ -3,11 +3,11 @@ import React from "react";
 function HomeScreen() {
   return (
     <>
-      <div className="w-full h-full flex justify-center items-center">
-        <div >
-          <h1 className="text-5xl font-bold text-textColor_Gray">
-            Welcome to P&A
-          </h1>
+      <div className="w-full h-full flex justify-center items-center font-sans">
+        <div className="flex flex-col text-center">
+          <h1 className="text-8xl font-bold text-textColor_Gray">Paulette</h1>
+          <p className="text-6xl font-bold text-textColor_Purple">&</p>
+          <h1 className="text-8xl font-bold text-textColor_Gray">Ángel</h1>
         </div>
       </div>
     </>
